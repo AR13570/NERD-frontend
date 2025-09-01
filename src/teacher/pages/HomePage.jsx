c3bf0d82d61f882e3ca2ada17f11a7b8d7834d46
arnav <arnav.rajesh07@gmail.com>
2025-09-01 18:01:48 +0530

    feat: homepage

--- src/teacher/pages/HomePage.jsx
+++ src/teacher/pages/HomePage.jsx
@@ -7,10 +7,19 @@ import CieQuestionGenerator from "../component/CieQuestionGenerator";
 import Modal from "@mui/joy/Modal";
 import ModalClose from "@mui/joy/ModalClose";
 import ModalDialog from "@mui/joy/ModalDialog";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
 
 const TeacherHomePage = () => {
   const navigate = useNavigate();
   const [show, setShow] = useState(false);
+  const [error, setError] = useState("");
+  const [file, setFile] = useState(null);
+  const [buttonshow, setButtonshow] = useState(false);
+  const [Generating, setGenerating] = useState(false);
+  const [response, setresponse] = useState("");
   useEffect(() => {
     const userType = localStorage.getItem("userType");
     if (userType == null || userType != "teacher") {
@@ -22,9 +31,82 @@ const TeacherHomePage = () => {
   }, []);
   return (
     <div className="bg-white h-full w-full">
+      <Modal open={buttonshow} onClose={() => setButtonshow(false)}>
+        <ModalDialog>
+          <div>
+            <div>
+              <div className="text-xl font-bold flex justify-center">
+                Bulk upload
+              </div>
+              <div className="flex flex-row gap-x-2 mt-4"></div>
+            </div>
+            <div className="text-base font-medium mb-2">Upload files</div>
+            Upload a csv file with student details to register them
+            <input
+              className="flex justify-center mb-4"
+              type="file"
+              accept=".csv"
+              onChange={(event) => {
+                setFile(event.target.files[0]);
+              }}
+            />
+            <Button
+              className="border border-slate-400 rounded-full px-3 py-2 text-base font-medium bg-[#18122B] text-white  flex justify-center w-full my-4"
+              onClick={async () => {
+                const formData = new FormData();
+                formData.append("file", file);
+                setError("");
+                try {
+                  setGenerating(true);
+                  const response = await axios.post(
+                    "http://localhost:8000/student/register/bulk",
+                    formData
+                  );
+                  setresponse(response);
+                  console.log(response);
+                } catch (error) {
+                  console.log(error);
+                  setError("Error: " + error.response.data.detail);
+                } finally {
+                  setGenerating(false);
+                }
+              }}
+              disabled={Generating}
+            >
+              {Generating ? "Uploading..." : "Upload CSV"}
+            </Button>
+            {error != "" && (
+              <p className="text-red-500 font-medium flex justify-center my-4">
+                {error}
+              </p>
+            )}
+            {response && (
+              <div className="text-green-400 text-sm font-medium text-center">
+                Students registed Successfully
+              </div>
+            )}
+          </div>
+          <ModalClose />
+        </ModalDialog>
+      </Modal>
       <div className="flex h-full">
-        <div className="flex w-[450px] h-full bg-[#18122B]  p-4">
+        <div className="flex w-[450px] h-full bg-[#18122B]  p-4 overflow-y-auto">
           <div className="flex flex-col">
+            <Typography class="text-xl font-extrabold text-slate-100">
+              Bulk registration
+            </Typography>
+            <Typography class="text-base text-slate-100 my-3 content-between">
+              Register students from a CSV file
+            </Typography>
+            <Button
+              class="mb-4 bg-white text-base py-2 text-violet-950 font-bold rounded"
+              onClick={() => {
+                setButtonshow(true);
+              }}
+            >
+              Register
+            </Button>
+
             <Typography class="text-xl font-extrabold text-slate-100">
               Generate Questions and Answers
             </Typography>
